a2cd47f06b3d492f81c070b914ab305d1fb8f331
WHITE13452 <1546936584@qq.com>
2025-06-16 00:53:28 +0800

    update sharedInformerFactory

--- Informer-demo/pkg/handlers/podhandler.go
+++ Informer-demo/pkg/handlers/podhandler.go
@@ -20,3 +20,20 @@ func (p *PodHandler) OnDelete(obj interface{}) {
 	// Handle pod deletion logic here
 	fmt.Println("PodHandler OnDelete: ", obj.(*v1.Pod).Name)
 }
+
+type NewPodHandler struct {
+
+}
+
+func (n *NewPodHandler) OnAdd(obj interface{}, isInInitialList bool) {
+	// Handle pod addition logic here
+	fmt.Println("NewPodHandler OnAdd: ", obj.(*v1.Pod).Name)
+}
+func (n *NewPodHandler) OnUpdate(oldObj, newObj interface{}) {
+	// Handle pod update logic here
+	fmt.Println("NewPodHandler OnUpdate: ", oldObj.(*v1.Pod).Name, " -> ", newObj.(*v1.Pod).Name)
+}
+func (n *NewPodHandler) OnDelete(obj interface{}) {
+	// Handle pod deletion logic here
+	fmt.Println("NewPodHandler OnDelete: ", obj.(*v1.Pod).Name)
+}
